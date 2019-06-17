e6e4931ac35d43a7d73968e6763a4796d934beeb
Paige Gorry <paigeegorry@gmail.com>
2019-06-17 11:50:21 -0700

    reformatted the affiliations and quests data; added default case to update the obj keys with other labels, values, photo, and name

--- services/infoScraper.js
+++ services/infoScraper.js
@@ -2,7 +2,7 @@ const { parse } = require('node-html-parser');
 const request = require('superagent');
 // const nameScraper = require('./nameScraper');
 
-const cleanUp = ({ labels, values }) => {
+const reformatData = ({ labels, values, photoInfo, name }) => {
   const obj = {};
   labels.map((label, i) => {
     if(label === 'Appearances') {
@@ -11,37 +11,38 @@ const cleanUp = ({ labels, values }) => {
         if(val === ' title=') return appearances[i + 1];
       }).filter(str => str);
     }
+    else if(label === 'Quests') {
+      const quests = values[i].innerHTML.split('"');
+      obj.quests = quests.map((val, i) => {
+        if(val === ' title=') return quests[i + 1];
+      }).filter(str => str);
+    }
+    else if(label === 'Affiliation') {
+      const affiliations = values[i].text.split('  ');
+      obj.affiliations = affiliations.filter(aff => !aff.includes('('));
+    }
+    else {
+      obj[label.toLowerCase()] = values[i].text;
+    }
+    obj.photo = photoInfo;
+    obj.name = name;
+    // console.log(label);
   });
   console.log(obj);
 };
 
 const infoScraper = () => {
-  return request.get('https://dragonage.fandom.com/wiki/Cremisius_Aclassi')
+  return request.get('https://dragonage.fandom.com/wiki/Iron_Bull')
     .then(res => res.text)
     .then(parse)
     .then(html => {
       const labels = html.querySelectorAll('.pi-data-label').map(l => l.childNodes[0].rawText);
       const values = html.querySelectorAll('div .pi-data-value');
       const photoInfo = html.querySelectorAll('.pi-image-thumbnail')[0].rawAttrs.split('"')[1];
-      // const list = html.document.querySelectorAll('div.pi-data-value.pi-font');
-      // console.log(list);
-      // return [labels, values, photoInfo];
-      // if last label = Appearances?
-      // how to map thru each childNode when it is technically an obj?
-      // console.log(values[values.length - 1].innerHTML);
-      // console.log(values[values.length - 1].childNodes[0].childNodes[0].childNodes[0].rawText);
-      return { labels, values };
+      const name = html.querySelectorAll('.selflink')[0].text;
+      return { labels, values, photoInfo, name };
     })
-    .then(cleanUp);
-  // .then(organizeInfo);
-};
-
-const organizeInfo = ([labels, values, photoInfo]) => {
-  if(labels[labels.length - 1] === 'Appearances') {
-    const digging = values[values.length - 1];
-    console.log(digging);
-    // console.log(Object.entries(digging)[0][1][1][0].childNodes);
-  }
+    .then(reformatData);
 };
 
 infoScraper();
