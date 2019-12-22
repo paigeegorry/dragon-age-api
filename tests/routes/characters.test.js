419b25c88cca3edeab2e6447fbb04923b60872f0
Paige Gorry <paigeegorry@gmail.com>
2019-12-21 20:37:50 -0800

    added tests for all routes advertised

--- tests/routes/characters.test.js
+++ tests/routes/characters.test.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
-const seedData = require('../seedData');
+const { seedData, characters } = require('../seedData');
 
-describe.skip('characters routes', () => {
+const BASE_URL = '/api/v1/characters';
 
-  //TODO: mock api call for test
+describe('characters routes', () => {
   beforeAll(() => {
     return mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
       .then(() => seedData());
@@ -19,9 +19,49 @@ describe.skip('characters routes', () => {
 
   it('can get all characters', () => {
     return request(app)
-      .get('/api/v1/characters')
+      .get(BASE_URL)
       .then(res => {
-        expect(res.body).toHaveLength();
+        expect(res.body).toHaveLength(5);
       });
   });
+  it('can get a random character', () => {
+    return request(app)
+      .get(`${BASE_URL}/random`)
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+  it('can get a character by its id', () => {
+    return request(app)
+      .get(`${BASE_URL}/${characters[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({ ...characters[0], __v: 0 });
+      });
+  });
+  describe('queries', () => {
+    it('can get a character by name', () => {
+      return request(app)
+        .get(`${BASE_URL}?name=Cassandra`)
+        .then(res => {
+          expect(res.body).toEqual([characters[0]]);
+        });
+    });
+    
+    it('can get a character by appearance', () => {
+      return request(app)
+        .get(`${BASE_URL}?appearances=Inquisition`)
+        .then(res => {
+          expect(res.body).toHaveLength(4);
+        });
+    });
+
+    it('can get a character by quest', () => {
+      return request(app)
+        .get(`${BASE_URL}?quests=${characters[0].quests[0]}`)
+        .then(res => {
+          expect(res.body).toEqual([characters[0]]);
+        });
+    });
+  });
+
 });
--- tests/seedData.js
+++ tests/seedData.js
@@ -1,11 +1,112 @@
 const Character = require('../lib/models/Character');
 
-const characters = [];
+const characters = [
+  {
+    '_id': '5d0915d1fec3ff91e271a5f7',
+    'photo': 'https://vignette.wikia.nocookie.net/dragonage/images/c/c9/Cass5.png/revision/latest/scale-to-width-down/350?cb=20150407154623',
+    'name': 'Cassandra Pentaghast',
+    'quests': [
+      'Guilty Pleasures',
+      'Promise of Destruction',
+      'The Ideal Romance',
+      'Unfinished Business'
+    ],
+    'appearances': [
+      'Dragon Age: Dawn of the Seeker',
+      'Dragon Age II',
+      'Dragon Age: Inquisition',
+      'Heroes of Dragon Age'
+    ]
+  },
+  {
+    '_id': '5d0915d1fec3ff91e271a630',
+    'photo': 'https://vignette.wikia.nocookie.net/dragonage/images/2/2a/Cullen_Profile_2a.png/revision/latest/scale-to-width-down/350?cb=20150405013511',
+    'name': 'Cullen Rutherford',
+    'quests': [
+      'Perseverance',
+      'Before the Dawn',
+      'Happier Times'
+    ],
+    'appearances': [
+      'Dragon Age: Origins',
+      'The Darkspawn Chronicles',
+      'Dragon Age II',
+      'Heroes of Dragon Age',
+      'Dragon Age: Inquisition'
+    ]
+  },
+  {
+    '_id': '5d0dbbf12e7afe17512028b3',
+    'name': 'Cole',
+    'photo': 'https://vignette.wikia.nocookie.net/dragonage/images/e/e5/Cole_Profile_Dimmed.jpg/revision/latest?cb=20150403230741',
+    'quests': [
+      'The Forgotten Boy',
+      'Subjected to His Will'
+    ],
+    'appearances': [
+      'Dragon Age: Asunder',
+      'Dragon Age: Inquisiton',
+      'Heroes of Dragon Age'
+    ]
+  },
+  {
+    '_id': '5d0915d1fec3ff91e271a5b8',
+    'photo': 'https://vignette.wikia.nocookie.net/dragonage/images/8/8b/Josephine-DAIProfile2.png/revision/latest/scale-to-width-down/350?cb=20150404051723',
+    'name': 'Josephine Montilyet',
+    'quests': [
+      'Of Somewhat Fallen Fortune',
+      'Heraldry From a Herald',
+      'An Unexpected Engagement'
+    ],
+    'appearances': [
+      'Dragon Age: Inquisition',
+      'Trespasser',
+      'Heroes of Dragon Age'
+    ]
+  },
+  {
+    '_id': '5d0915d1fec3ff91e271a68d',
+    'photo': 'https://vignette.wikia.nocookie.net/dragonage/images/5/59/Varric.jpg/revision/latest/scale-to-width-down/350?cb=20110217185229',
+    'name': 'Varric Tethras',
+    'quests': [
+      'Questions and Answers',
+      'An Update',
+      'Family Matter',
+      'Friendly Concern',
+      'A Story Being Told',
+      'Plans for the Future',
+      'Varric\'s Apology',
+      'The Tethras Signet Ring',
+      'The Storm and what Came Before It',
+      'A Small Problem',
+      'Haunted',
+      'Closure',
+      'An Anniversary',
+      'Literary Theft',
+      'Seeing Red',
+      'Well, Shit'
+    ],
+    'appearances': [
+      'Dragon Age II',
+      'Dragon Age: The Silent Grove',
+      'Dragon Age: Those Who Speak',
+      'Dragon Age: Until We Sleep',
+      'Dragon Age: Inquisition',
+      'Trespasser',
+      'Dragon Age: Knight Errant',
+      'Heroes of Dragon Age'
+    ]
+  }
+];
 
-module.exports = async() => {
+const seedData = async() => {
   const characterSeed = await characters.map(character => {
     return Character.create(character);
   });
   return characterSeed;
 };
 
+module.exports = {
+  seedData,
+  characters
+};
